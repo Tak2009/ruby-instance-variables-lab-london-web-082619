e9289df94786a6358ef4dc535504478f967be01a
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-27 19:34:49 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+end  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  lassie.name
+  
+  puts lassie.name
